3708883e18e6e6864a85cb7b863713d367a82bdb
Wellington <well_yogui@hotmail.com>
2017-06-13 17:07:08 -0300

    tela de config (fazer a funcionalidade), splashscreen

--- app/src/main/java/wellington/br/com/trucocounter/Config_activity.java
+++ app/src/main/java/wellington/br/com/trucocounter/Config_activity.java
@@ -1,16 +1,42 @@
 package wellington.br.com.trucocounter;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class Config_activity extends AppCompatActivity {
+
+    @Bind(R.id.tvNameTheyConfig)
+    EditText tvNameTheyConfig;
+    @Bind(R.id.tvNameTheyLabel)
+    TextView tvNameTheyLabel;
+    @Bind(R.id.tvNameWeLabel)
+    TextView tvNameWeLabel;
+    @Bind(R.id.tvNameWeConfig)
+    EditText tvNameWeConfig;
+    @Bind(R.id.tvPontMax)
+    EditText tvPontMax;
+    @Bind(R.id.tvPontMaxLabel)
+    TextView tvPontMaxLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
         ButterKnife.bind(this);
+
+        Typeface type = Typeface.createFromAsset(getAssets(), "fonts/chalk.ttf");
+
+        tvNameTheyLabel.setTypeface(type);
+        tvNameWeLabel.setTypeface(type);
+        tvPontMax.setTypeface(type);
+        tvPontMaxLabel.setTypeface(type);
+        tvNameTheyConfig.setTypeface(type);
+        tvNameWeConfig.setTypeface(type);
     }
 }
--- app/src/main/java/wellington/br/com/trucocounter/SplashScreen.java
+++ app/src/main/java/wellington/br/com/trucocounter/SplashScreen.java
@@ -1,6 +1,7 @@
 package wellington.br.com.trucocounter;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,12 +9,27 @@ import butterknife.ButterKnife;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private int SPLASH_SCREEN_TIME = 2500;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         ButterKnife.bind(this);
 
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                startMainActivity();
+            }
+        }, SPLASH_SCREEN_TIME);
+
+
+    }
+
+
+    private void startMainActivity() {
         Bundle bundle = new Bundle();
         if (bundle != null){
 
