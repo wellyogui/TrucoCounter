e66ac79993279f30be3ca689f54b00a784283d38
Wellington <well_yogui@hotmail.com>
2017-06-13 15:30:12 -0300

    imagem do botão (ajustar tamanho)

--- app/src/main/java/wellington/br/com/trucocounter/Config_activity.java
+++ app/src/main/java/wellington/br/com/trucocounter/Config_activity.java
@@ -3,11 +3,14 @@ package wellington.br.com.trucocounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class Config_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
+        ButterKnife.bind(this);
     }
 }
